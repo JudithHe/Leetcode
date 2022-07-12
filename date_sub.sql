0e3144e748564397dd233877e3bdd9ba1a3e5537
Judith He <44380995+JudithHe@users.noreply.github.com>
2022-07-12 16:36:53 +0800

    Update date_sub.sql
    https://blog.csdn.net/wzl1217333452/article/details/122122693

--- date_sub.sql
+++ date_sub.sql
@@ -10,5 +10,18 @@ insert into calc values(203,'2017/1/2');
 insert into calc values(203,'2017/1/3');
 insert into calc values(203,'2017/1/4');
 
-select uid,loadtime,row_number()over(partition by UID order by loadtime) sort
+select uid, loadtime, row_number() over(partition by uid order by loadtime) as sort
 from calc;
+
+select uid, date_sub(loadtime, sort) as same_start, count() as daycount
+from (select uid, loadtime, row_number() over(partition by uid order by loadtime) as sort
+      from calc) t;
+group by uid,same_start
+
+Select uid, max(daycount) as maxdays
+from (select uid, date_sub(loadtime, sort) as same_start, count() as daycount
+    from (select uid, loadtime, row_number() over(partition by uid order by loadtime) as sort
+          from calc) t;
+    group by uid,same_start) b
+group by uid
+
